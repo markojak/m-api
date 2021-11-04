6a644137a153904e8995453650019c412d81c800
Curly Brackets <water.curly@outlook.com>
2021-11-04 21:59:54 +0800

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { join, resolve } from "path";
+import fs from "fs";
+
+const extensionDirectory = resolve(process.cwd(), "extensions");
 
 type Data = {
-  name: string
-}
+  content: string;
+};
+
+type Error = {
+  error: string;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Error>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "GET") {
+    const { slug } = req.query;
+
+    const ext = fs.readFileSync(
+      join(
+        extensionDirectory,
+        Array.isArray(slug) ? slug[0] : slug,
+        "README.md"
+      ),
+      {
+        encoding: "utf-8",
+      }
+    );
+    return res.status(200).json({
+      content: ext,
+    });
+  } else {
+    return res.status(404).json({
+      error: "Not Support Method",
+    });
+  }
 }
