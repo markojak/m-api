b78516a6027168621e388f0bfaf1bc394ca6e85d
Curly Brackets <water.curly@outlook.com>
2021-11-04 22:47:35 +0800

    ts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,7 +1,8 @@
 const { join, resolve } = require("path");
 const { readFileSync } = require("fs");
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
   const templateDirectory = resolve(process.cwd(), "email");
   const emailTemplate = readFileSync(
