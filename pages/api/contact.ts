2001558ddccb0db982d275195491bf3fbb5c4533
Curly Brackets <water.curly@outlook.com>
2021-11-05 09:33:08 +0800

    reprouction

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,10 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getTemplate } from "../../lib/api";
+import { resolve, join } from "path";
+import { readFileSync } from "fs";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { slug } = req.query;
-
-  res.status(200).json({
-    content: getTemplate(slug),
-  });
+type Data = {
+  updated: string[];
+  deleted: string[];
 };
+
+type Error = {
+  error: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | Error>
+) {
+  const { slugs } = req.query;
+
+  const data: Data = {
+    deleted: [],
+    updated: [],
+  };
+
+  (typeof slugs === "string" ? [slugs] : slugs).forEach((slug: string) => {
+    const templateDirectory = resolve(process.cwd(), "extensions");
+    const emailTemplate = readFileSync(
+      join(templateDirectory, slug, "README.md"),
+      "utf8"
+    );
+    if (!emailTemplate) {
+      data.deleted.push(slug);
+    } else {
+      data.updated.push(emailTemplate);
+    }
+  });
+  return res.status(200).json(data);
+}
