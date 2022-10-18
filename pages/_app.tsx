3226c843f9899cf008139aa7b0ae3e3841e5e57a
Marko Jak <marko@markojak.com>
2022-10-18 13:58:14 -0500

    Adding Umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,20 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="lazyOnload"
+          />
+        )}
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
